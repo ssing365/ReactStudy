6ee417d3a7ca48d78fb71c66b5bef7ba0adf89d2
Hosung <ssing365@naver.com>
2025-01-04 23:01:09 +0900

    Chatting Project Design

--- project03-chatting/src/components/ChatStart.jsx
+++ project03-chatting/src/components/ChatStart.jsx
@@ -12,13 +12,16 @@ function ChatStart() {
 
   return (
     <>
-      <h2>톡토오톡</h2>
+      <h2>Realtime DB Chatting</h2>
+      <hr />
       {/* 앞에서 생성한 Ref변수를 <input>태그에 추가하여 DOM에 접근 */}
-      방명 :{" "}
+      방 이름 :{" "}
       <input type="text" name="roomId" id="" value="room1" ref={refRoom} />{" "}
       <br />
+      <br />
       대화명 : <input type="text" name="userId" id="" ref={refId} /> <br />
-      <button onClick={openChatWin}>채팅시작</button>
+      <br />
+      <button onClick={openChatWin}>채팅 시작</button>
     </>
   );
 }
